a22fbc6593a34c2bd465354bc41d6e1a6f0f4a3e
vidhigupta17 <vidhigupta1500@gmail.com>
2024-01-28 14:59:52 +0530

    Initialization Of Project-Added overallattendance screen Added Custom Widgets- CustomAppBar,SubjectAttendanceCard,AttendaceCard

---
+++ lib/Screens/OverAllAttendance.dart
@@ -0,0 +1,68 @@
+import 'package:edumarshal/Widget/AttendanceCard.dart';
+import 'package:edumarshal/Widget/SubjectAttendanceCard.dart';
+import 'package:flutter/material.dart';
+
+import '../Widget/CustomAppBar.dart';
+
+class OverAllAttd extends StatefulWidget {
+  const OverAllAttd({super.key});
+
+  @override
+  State<OverAllAttd> createState() => _OverAllAttdState();
+}
+
+class _OverAllAttdState extends State<OverAllAttd> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 236, 255, 251),
+       appBar: CustomAppBar(
+        userName: 'Vidhi Gupta', 
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              // Container(),
+              AttendanceCard(
+                  title: "Overall Attendance",
+                  description: "including all subjects and labs."),
+                 Container(
+                alignment: Alignment.centerLeft, // Align text to the left
+                margin: EdgeInsets.only(left: 16.0), // Add left margin for the text
+                child: Text(
+                  "All Subject",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+              SubjectAttendanceCard(
+                  subjectName: "Mathematics - IV",
+                  attendedClasses: 60,
+                  totalClasses: 100),
+              SubjectAttendanceCard(
+                  subjectName: "Mathematics - IV",
+                  attendedClasses: 90,
+                  totalClasses: 100),
+              SubjectAttendanceCard(
+                  subjectName: "Mathematics - IV",
+                  attendedClasses: 80,
+                  totalClasses: 100),
+              SubjectAttendanceCard(
+                  subjectName: "Mathematics - IV",
+                  attendedClasses: 70,
+                  totalClasses: 100),
+                     SubjectAttendanceCard(
+                  subjectName: "Mathematics - IV",
+                  attendedClasses: 90,
+                  totalClasses: 100),
+                     SubjectAttendanceCard(
+                  subjectName: "Mathematics - IV",
+                  attendedClasses: 90,
+                  totalClasses: 100),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Widget/SubjectAttendanceCard.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+
+class SubjectAttendanceCard extends StatelessWidget {
+  final String subjectName;
+  final int attendedClasses;
+  final int totalClasses;
+
+  SubjectAttendanceCard({
+    required this.subjectName,
+    required this.attendedClasses,
+    required this.totalClasses,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double attendancePercentage = (attendedClasses / totalClasses) * 100;
+
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 5.0,
+            spreadRadius: 2.0,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  subjectName,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  'Attendance: $attendedClasses / $totalClasses',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 16.0),
+          CircularPercentIndicator(
+            radius: 30.0,
+            lineWidth: 10.0,
+            percent: attendancePercentage / 100,
+            center: Text(
+              '${attendancePercentage.toStringAsFixed(1)}%',
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            backgroundColor: Colors.white,
+            progressColor: Color.fromARGB(255, 22, 192, 28),
+          ),
+        ],
+      ),
+    );
+  }
+}
